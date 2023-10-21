501130bfd68a925c56d7521985508855710b12f1
Ramil Gojayev <gojayram@fit.cvut.cz>
2023-10-21 08:29:59 +0200

    Added billboard being processed from .env -> MAIN_BILLBOARD_ID

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -8,13 +8,14 @@ import getProducts from "@/actions/get-products";
 export const revalidate = 0;
 
 const HomePage = async () => {
-  const billboard = await getBillboard("9e5a51a6-a621-4609-a8ee-7bb64476cc8f");
+  const id = process.env.MAIN_BILLBOARD_ID ? process.env.MAIN_BILLBOARD_ID : "";
+  const billboard = await getBillboard(id);
   const products = await getProducts({ isFeatured: true });
 
   return (
     <Container>
       <div className="space-y-10 pb-10">
-        <Billboard data={billboard} />
+        {billboard && <Billboard data={billboard} />}
         <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
           <ProductList title="Featured products" items={products} />
         </div>
